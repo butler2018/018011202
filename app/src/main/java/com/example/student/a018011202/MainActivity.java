8f62df6823e8b5f33eb2a93d087bc531e8b94785
calvin <butler2018@hotmail.com>
2018-01-12 10:37:17 +0800

    Initial commit

---
+++ app/src/main/java/com/example/student/a018011202/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.student.a018011202;
+
+import android.app.DownloadManager;
+import android.app.VoiceInteractor;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void click1(View v)
+    {
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+        StringRequest request = new StringRequest("http://data.ntpc.gov.tw/od/data/api/BF90FA7E-C358-4CDA-B579-B6C84ADC96A1?$format=json",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("NET", response);
+                        try {
+                            JSONArray array = new JSONArray(response);
+                            JSONObject obj = array.getJSONObject(0);
+                            String str = obj.getString("district");
+                            Log.d("NET", str);
+                            String str1 = obj.getString("address");
+                            Log.d("NET", str1);
+                            String tel = obj.getString("tel");
+                            Log.d("NET", tel);
+                            String hours = obj.getString("opening_hours");
+                            Log.d("NET", hours);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        queue.add(request);
+        queue.start();
+    }
+
+}
+
+
